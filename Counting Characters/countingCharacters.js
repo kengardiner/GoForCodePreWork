69d9c991a790e3286d0780993107b42aa45858ca
Ken Gardiner <ken_gardiner@yahoo.com>
2017-07-06 15:17:10 -0400

    Counting chars 3

--- Counting Characters/countingCharacters.js
+++ Counting Characters/countingCharacters.js
@@ -19,3 +19,17 @@ function countingCharacters2(stringToCount, characterToFind){
     console.log("Number of times the character appears: " + 
                   characterCount);
 }
+
+function countingCharacters3(str, search){    
+    var count = 0;
+    var numChars = search.length;
+    
+    var lastIndex = str.length - numChars;
+    for (var index = 0; index <= lastIndex; index++){
+       var current = str.substring(index, index + numChars);
+        if (current == search){            
+            count++;
+        }
+    }
+    return count;
+}
